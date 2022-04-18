3c3206c0bae5ce59262a110112353926e21f5a00
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-04-18 19:21:48 -0400

    commit tree e8134cc64e2ebddd98f4e942c0d8857e86ac6f78

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isOn, setIsOn] = useState(false);
+  
+  function handleCartEvent() {
+    setIsOn((isOn) => !isOn )
+  }
+
+  const cartClass = isOn ? 'in-cart' : ''
+
   return (
-    <li className="">
+    <li className={ cartClass }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={ handleCartEvent }>{ isOn ? 'Remove From Cart' : 'Add to Cart'}</button>
     </li>
   );
 }
